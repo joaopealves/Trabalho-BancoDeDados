a6e69cd1807fd710bf82a0a34fba19e4375ec078
João Pedro Alves <jotalmeida007@hotmail.com>
2020-09-23 18:47:54 -0300

    Fechar trabalho

--- Documentos/scripts.sql
+++ Documentos/scripts.sql
@@ -90,7 +90,7 @@ BEGIN
     dt_desligamento)
     
     VALUES 
-	(old.matricula,
+	  (old.matricula,
     old.nome,
     old.sobrenome,
     old.email,
@@ -106,35 +106,170 @@ select * from ex_empregados;
 -- Item 8
 
 DELIMITER $$
-  drop procedure if exists insertEmpregados $$
-    CREATE PROCEDURE 
+USE `bd_atividade1`$$
+CREATE PROCEDURE `selectGerentes` ()
+BEGIN
+	select d.nome, e.titulo from empregados as d join cargo as e on d.cargo_id = e.cargo_id where d.cargo_id in (2,7,10,14,15,19);
+END$$
 
-    insertEmpregados
-		(IN matricula INT ,
-		IN nome varchar(20),
-		IN sobrenome varchar(25),
-		IN email varchar(100),
-		IN telefone varchar(20), 
-		IN dt_admissao DATE,
-		IN cargo_id INT,
-		IN salario DECIMAL,  
-		IN gerente_mat INT,
-		IN department_id INT) 
+DELIMITER ;
+
+call selectGerentes;
+
+--Item 9
+
+DELIMITER $$
+USE `bd_atividade1`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `bd_atividade1`.`alterEmpregado` AFTER UPDATE ON `empregados` FOR EACH ROW
+BEGIN
+	INSERT INTO historico_empregado (matricula,nome,sobrenome,email,telefone,dt_admissao,salario,cargo_id,departamento_id,dt_alteracao,usuario_logado)
+    VALUES  (old.matricula,old.nome,old.sobrenome,old.email,old.telefone,old.dt_admissao,old.salario,old.cargo_id,old.department_id,now(),current_user);
 	
-    BEGIN 
+    INSERT INTO historico_empregado (matricula,nome,sobrenome,email,telefone,dt_admissao,salario,cargo_id,departamento_id,dt_alteracao,usuario_logado)
+    VALUES (new.matricula,new.nome,new.sobrenome,new.email,new.telefone,new.dt_admissao,new.salario,new.cargo_id,new.department_id,now(),current_user);
+END$$
+DELIMITER ;
 
-      INSERT INTO empregados VALUES 
-		(matricula,
-        nome,
-        sobrenome,
-        email,
-        telefone,
-        dt_admissao,
-        cargo_id,salario,
-        gerente_mat,
-        department_id); 
+update empregados set telefone = '61 4002-8922' where matricula = '455';
 
-    END $$  
-          
-DELIMITER ; 
+drop trigger alterEmpregado;
 
+select * from historico_empregado;
+
+--Item 10
+
+DELIMITER $$
+USE `bd_atividade1`$$
+CREATE FUNCTION idadeEmpregados (ID smallint)
+
+RETURNS INT
+
+BEGIN
+declare idade INT;
+
+set idade = (select YEAR(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(dt_admissao))) as idade FROM empregados where matricula = id);
+
+return idade;
+END $$
+DELIMITER ;
+
+select nome, idadeEmpregados(matricula) as idadeEmpregados from empregados;
+
+select dt_admissao from empregados;
+
+drop function idadeEmpregados;
+
+--Aprendi que não se deve usar a idade em um banco de dados, já que ela muda, o ideal é sempre colocar a data hehe
+
+--Item 11
+DELIMITER $$
+USE `bd_atividade1`$$
+CREATE FUNCTION contraCheque (ID smallint)
+
+RETURNS INT
+
+BEGIN
+declare notaEmissao INT;
+
+set notaEmissao = (SELECT sum(salario)  FROM empregados);
+
+return folha;
+END $$
+DELIMITER ;
+
+select contraCheque(matricula) as totalSalarios from empregados limit 1;
+
+drop function contraCheque;
+
+--Item 12
+
+-- Não consegui fazer
+
+--Item 13
+
+DELIMITER $$
+USE `bd_atividade1`$$
+CREATE PROCEDURE `quantidadeEmpregados` ()
+BEGIN
+	SELECT count(nome) as totalEmpregados FROM empregados limit 1;
+END$$
+DELIMITER ;
+
+call quantidadeEmpregados;
+
+--Item 14
+
+DELIMITER $$
+drop procedure if exists quantidadeEmpregados;
+USE `bd_atividade1`$$
+CREATE PROCEDURE `quantidadeEmpregados` (id smallint)
+BEGIN
+    SELECT count(*) as totalDependentes from dependentes where matricula = id;
+END$$
+DELIMITER ;
+
+
+call quantidadeEmpregados(555);
+
+select * from dependentes;
+
+--Item 15
+
+DELIMITER $$
+drop procedure if exists ajusteSalario;
+USE `bd_atividade1`$$
+CREATE PROCEDURE `ajusteSalario` ()
+BEGIN
+    update empregados set salario = (1.05*salario);
+END$$
+DELIMITER ;
+
+
+--Item 16
+
+SET SQL_SAFE_UPDATES = 0;
+call ajusteSalario;
+select nome, salario from empregados;
+
+-- Item 17
+
+DELIMITER $$
+USE `bd_atividade1`$$
+CREATE PROCEDURE aumentoVeteranos ()
+BEGIN
+
+	 UPDATE empregados SET salario = (1.20 * salario) where year(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(dt_admissao))) >=20;
+END
+$$ 
+DELIMITER ;
+
+--Item 18
+
+SET SQL_SAFE_UPDATES = 0;
+
+call aumentoVeteranos;
+
+select nome, salario from empregados where year(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(dt_admissao))) >=20;
+
+--Item 19
+
+select salario from empregados order by salario desc limit 1;
+
+--Item 20
+
+DELIMITER $$
+USE `bd_atividade1`$$
+CREATE FUNCTION quantidadeDependentes (ID smallint)
+
+RETURNS INT
+
+BEGIN
+declare quantidadeDependentes INT;
+
+set quantidadeDependentes = (SELECT count(*) as quantidadeDependentes from dependentes where matricula = id);
+
+return quantidadeDependentes;
+END $$
+DELIMITER ;
+
+select quantidadeDependentes(matricula) as Dependentes from empregados where matricula ='333';
--- README.md
+++ README.md
@@ -18,32 +18,32 @@
 dt_desligamento (data que foi deletado).
 Testar a trigger e apague os empregados com matricula 207 e 208 e teste a procedure com um empregado que foi incluido no exercício 4.(✔)
 
-8 - Criar uma procedure que exiba o nome de todos os empregados que são gerentes, essa procedure não precisa de parâmetros;
+8 - Criar uma procedure que exiba o nome de todos os empregados que são gerentes, essa procedure não precisa de parâmetros;(✔)
 
-9 - Criar uma trigger que após qualquer alteração na tabela empregados, os dados do empregado, antes e depois da alteração (update), devem ser gravados na tabela historico_empregado e também armazene os dados dt_alteracao (data do servidor) e o usuario logado no banco de dados.
+9 - Criar uma trigger que após qualquer alteração na tabela empregados, os dados do empregado, antes e depois da alteração (update), devem ser gravados na tabela historico_empregado e também armazene os dados dt_alteracao (data do servidor) e o usuario logado no banco de dados.(✔)
 
-10 - Criar uma função que exiba quantos anos o empregado trabalha na empresa.
+10 - Criar uma função que exiba quantos anos o empregado trabalha na empresa.(✔)
 
-11 - Criar uma função que retorne o total do salario da folha de pagamento da empresa, ou seja a soma dos salarios dos empregados.
+11 - Criar uma função que retorne o total do salario da folha de pagamento da empresa, ou seja a soma dos salarios dos empregados.(✔)
 
 12 - Criar uma procedure que selecione o nome, sobrenome, dt_admissao em formato dd/mm/YYYY, nome do cargo, nome do gerente, nome do departamento e
-salario do empregado.
+salario do empregado.(!)
 
-13 - Criar uma procedure que conte quantos empregados tem na empresa;
+13 - Criar uma procedure que conte quantos empregados tem na empresa;(✔)
 
-14 - Criar uma procedure que conte quantos dependentes tem o empregado, a procedure deverá receber como parâmetro a matricula do empregado.
+14 - Criar uma procedure que conte quantos dependentes tem o empregado, a procedure deverá receber como parâmetro a matricula do empregado.(✔)
 
-15 - Criar uma procedure que conceda um aumento de 5% em todos os salarios dos empregado, essa procedure não tem parâmetros.
+15 - Criar uma procedure que conceda um aumento de 5% em todos os salarios dos empregado, essa procedure não tem parâmetros.(✔)
 
-16 - Execute a procudere feita no exercício anterior;
+16 - Execute a procudere feita no exercício anterior;(✔)
 
-17 - Criar uma procedure que conceda um aumento de 20% para os empregados que tem mais de 20 anos na empresa.
+17 - Criar uma procedure que conceda um aumento de 20% para os empregados que tem mais de 20 anos na empresa.(✔)
 
-18 - Execute a procudere feita no exercício anterior;
+18 - Execute a procudere feita no exercício anterior;(✔)
 
-19 - Faça uma função que retorne o maior salario pago na empresa;
+19 - Faça uma função que retorne o maior salario pago na empresa;(✔)
 
-20 - Criar uma função que receba como parâmetro a matricula do empregado e conte quantos dependentes tem o empregado, caso o empregado não tenha depedente deverá retornar o valor 0;
+20 - Criar uma função que receba como parâmetro a matricula do empregado e conte quantos dependentes tem o empregado, caso o empregado não tenha depedente deverá retornar o valor 0;(✔)
 
 </h4>
 
